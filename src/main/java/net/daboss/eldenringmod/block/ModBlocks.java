6be8b49b4acc60d8f352de99c1f3b70ae5fb6cc0
theot <theot1856@magic.school.nz>
2025-11-21 15:28:40 +1300

    Initial commit

--- src/main/java/net/daboss/eldenringmod/block/ModBlocks.java
+++ src/main/java/net/daboss/eldenringmod/block/ModBlocks.java
@@ -2,9 +2,12 @@ package net.daboss.eldenringmod.block;
 
 import net.daboss.eldenringmod.EldenRingMod;
 import net.daboss.eldenringmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -13,19 +16,28 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import java.util.function.Supplier;
 
 public class ModBlocks {
-    public static final DeferredRegister BLOCKS =
+    public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(EldenRingMod.MOD_ID);
 
+    public static final DeferredBlock<Block> METEORIC_ORE = registerBlock("meteoric_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 4),
+                    BlockBehaviour.Properties.of().strength(3f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
 
-    private static <T extends Block> void registerBlock(String name, Supplier<T> block){
+    public static final DeferredBlock<Block> DEEPSLATE_METEORIC_ORE = registerBlock("deepslate_meteoric_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 5),
+                    BlockBehaviour.Properties.of().strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
+        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
     }
 
-    private static <T extends Block> void registerBlockWithItem(String name, DeferredBlock<T> block){
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
-    public static void register(IEventBus eventBus){
+    public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
 }
